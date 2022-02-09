bc760e5ab4e5c0f89d059f791786673277374022
wrojas2 <wiros2@gmail.com>
2022-02-08 21:42:34 -0500

    VARIABLES Y TIPOS DE DATOS 08-02-2022

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
-console.log("Mensaje");
-console.warn("Advertencia")
-console.error("Error");
-console.group("Agrupar Mensaje");
 
-    let datos = [
-        {nombre: "William", Edad: 45},
-        {nombre: "Miguel", Edad: 23},
-        {nombre: "Raul", Edad: 33},
-        {nombre: "Juan Carlos", Edad: 36},
-        {nombre: "Oscar", Edad: 29},
-    ]
-console.table(datos);
-console.groupEnd();
+/* *** TIPO DE DATO NULL Y UDIFINED ***
+let variableExistente= null; // tipo Objeto
+let variableNoExistente = undefined;    // tipo indefinido
 
-console.log ("%c Personalizado", "color:blue;font-family:cursive;font-size:20px");
+console.log(typeof (variableExistente));
+console.log(typeof (variableNoExistente));*/
+
+/* *** TIPO DE DATO BOOLEAN ***
+let soySuProfesor = true;
+let soyUnEstudiante = false;
+
+console.log (`Verdadero: ${soySuProfesor} Falso: ${soyUnEstudiante}`) 
+*/
+
+/* *** TIPO DE DATOS NUMBER ***
+let edad = 45;
+let altura = 1.75;
+let moneda = Intl.NumberFormat("de-DE", {style: "currency", currency: "COP"}).format(1000);
+console.log(`
+    Edad:${edad},
+    Altura:${altura},
+    Moneda:${moneda}
+\n`);  */
+
+/* *** TIPO DE DATOS STRING  ***
+let primerNombre = "William";
+let segundoNombre = '';
+let apellidos = `Rojas Rojas`;
+
+console.log(primerNombre,segundoNombre,apellidos);
+console.log(primerNombre+""+segundoNombre+' "'+apellidos+"\n'"+'"');
+console.log(primerNombre+' "'+"'"+toString(555).value+"\\n'"+'"')
+console.log(primerNombre.concat(apellidos)); // solo para string
+console.log(`${primerNombre} "'${apellidos}'"`);
+
+-/- Dato almacenado en la cache del navegador 'localstorage' ***
+localStorage.setItem("Dato","libro de cocina");
+let recuperar = localStorage.getItem("Dato");
+console.log(recuperar); 
+
+-/- Contantes en JS
+const libros = "Libro de Cocina";
+let libros = "Matemática";
+console.log(libros);
+
+-/- Declaracion de tipo de datos heredado del objeto
+let numero = new Number (1000000);
+let nombre = new String ('Hola');
+console.log (numero);
+console.log (nombre);
+
+*/
+
 
 
    
