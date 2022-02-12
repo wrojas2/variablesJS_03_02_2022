6def2a8e44b9d9b35817eede79b7134ec23a2cd5
wrojas2 <wiros2@gmail.com>
2022-02-12 09:19:20 -0500

    TIPO DE DATOS BASADOS EN OBJETOS 10-02-2022

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
 
-/* *** TIPO DE DATO NULL Y UDIFINED ***
+/* *** TIPO DE DATO SET *** 
+const data = new Set(); // Solo acepta 5 datos
+data.add("William");
+data.add("Rojas")
+console.log(data)
+*/
+
+/* *** TIPO DE DATO MAPS ***  
+const lista = new Map ();
+lista.set("Nombre","William" );
+lista.set("Apellido","Rojas" );
+console.log(lista.get("Nombre"));  // Nombre Variable
+console.log(lista.has("Nombre"));  // Vr Verdadero o falso
+console.log(lista.keys());
+console.log(lista.values());
+*/
+
+/* *** TIPO DE DATO OBJETO ***
+const informacion = {
+    personal:{
+        edad: 45,
+        nombre:"William Rojas",
+        altura:1.75
+    },
+    vivienda:
+           {
+        direccion: "Calle 17 # 1W -50",
+        pais:"Colomnbia",
+        departamento: "Santander"
+
+    }, 
+    mostrarEdad(){ 
+        return `${this.personal.nombre}*${this.personal.edad}}`;
+    }
+};
+informacion.personal.nombre = "William Rojas Rojas";
+console.log(informacion.mostrarEdad());
+console.log(informacion);  */
+
+/* *** TIPO DE DATO NULL Y UNDIFINED ***
 let variableExistente= null; // tipo Objeto
 let variableNoExistente = undefined;    // tipo indefinido
 
